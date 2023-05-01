59fce9d27e3e0d11af5f47d43daadbd9c1c0d673
Misty Becker <mistyb01@uw.edu>
2023-05-01 13:40:05 -0700

    created types Book and BookEntry

---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export type Book = {
+    id: string,
+    title: string,
+    author: string,
+    pageCount: number,
+    // yearPublished: number,
+    // category: string[]
+}
+
+export type BookEntry = {
+    // the id is how book entries are connected w books.. 
+    // used an indexed access type 
+    id: Book['id'], 
+    status: "current" | "finished" | "tbr", // this is called a 'literal type'!
+    pagesRead: number,
+    userRating: 1 | 2 | 3 | 4 | 5, // literal type
+}
