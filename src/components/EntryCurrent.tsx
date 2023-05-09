2b3ba22ff1fefcafb2f70993c40e80fccaff9f1e
Misty Becker <mistyb01@uw.edu>
2023-05-08 18:43:16 -0700

    EmptyListMessage component, and static update button

--- src/components/EntryCurrent.tsx
+++ src/components/EntryCurrent.tsx
@@ -3,27 +3,26 @@ import LinearProgress from '@mui/material/LinearProgress';
 import Box from "@mui/material/Box";
 import { Typography } from "@mui/material";
 import Divider from '@mui/material/Divider';
+import Button from "@mui/material/Button";
 
 const EntryCurrent = ({title, authors, pageCount, pagesRead, dateStarted}: BookEntry) => {
   let progressBarVal = (pagesRead / pageCount) * 100;
 
   return (
-    <>
-      {/* <img src={image} className="tracker-entry-img"/> */}
-        
+    <>        
         <Box>
           <Typography variant="entryHeader">{title}</Typography>
           <Typography>{authors.join(', ')}</Typography>
         </Box>
         <Divider variant="middle" />
         {dateStarted && <p>Started reading {dateStarted}</p>}
-        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
-          <Box sx={{ width: '100%', mr: 1 }}>
+        <Box sx={{ display: "flex", alignItems: "center"}}>
+        <Button size="small" variant="outlined" sx={{ width: "min-content"}}>update</Button>
+          <Box sx={{ width: '100%', ml: 1 }}>
+            <Typography variant="body2">{pagesRead} / {pageCount} pages read</Typography>
             <LinearProgress variant="determinate" sx={{ borderRadius: "5px", height: "10px"}} value={progressBarVal} />
           </Box>
-          <Typography variant="body2">{pagesRead} / {pageCount} pages read</Typography>
         </Box>
-        {/* <progress id="pages" max={pageCount} value={pagesRead}></progress> */}
     </>
   )
 }
--- src/components/TrackerList.tsx
+++ src/components/TrackerList.tsx
@@ -1,3 +1,4 @@
+import { Box, Typography } from "@mui/material";
 import { BookEntry } from "../types";
 import EntryCurrent from "./EntryCurrent";
 import EntryFinished from "./EntryFinished";
@@ -10,13 +11,27 @@ interface Props {
     listType: "current" | "finished" | "tbr"
 }
 
+const EmptyListMessage = ({listType}:{listType:string}) => {
+    if (listType === "tbr") listType = "to be read";
+    return (
+        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", mt: "3rem", textAlign: "center"}}>
+            <Typography variant="h5">Nothing here yet!</Typography>
+            <Typography variant="body2">
+                {listType === "current" ? <>Books you are <strong>currently reading</strong></>
+                : listType === "finished" ? <>Books you have <strong>finished</strong></>
+                : <>Books you <strong>plan to read</strong></>} will appear in this tab.
+            </Typography>
+        </Box>
+    )
+}
+
 const TrackerList : React.FC<Props> = ({userBooks, listType}) => {
-    if (!userBooks) {
-        return <p>No books yet!</p>
-    }
+    if (!userBooks) { return (<EmptyListMessage listType={listType}/>) }
+
     const filteredBooks = userBooks.filter((book) => book.status === listType);
 
-    if (filteredBooks.length === 0) { return <p>No books yet!</p>}
+    if (filteredBooks.length === 0) { 
+        return (<EmptyListMessage listType={listType}/>) }
 
     return (
         <Stack spacing={3}>
